d899872bf060249db4b3df9898d923763b21652f
marchersimon <marchersimon@zohomail.eu>
2020-12-11 23:18:08 +0100

    Reorganize init.el and make completely portable

--- init.el
+++ init.el
@@ -1,53 +1,60 @@
+;; Set up packages
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+(package-refresh-contents)
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
+
 ;; Load all custom auctex configuration
 (load "~/.emacs.d/auctex_config.el")
 
-;;Load theme
+;; UI settings
 (load-theme 'misterioso)
-
-;; Disable UI elements
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
+(global-display-line-numbers-mode 1)
+(setq inhibit-startup-message t
+      inhibit-startup-echo-area-message t)
+(setq-default cursor-type 'bar)
+(set-cursor-color "#ffffff")
 
-;; Set keybindings
+;;; Disable line numbering in certain modes
+(dolist (mode '("eshell" "org" "inferior-emacs-lisp"))
+  (add-hook (intern (concat mode "-mode-hook")) (lambda () (display-line-numbers-mode 0))))
+
+;; Editing settings
+(setq next-line-add-newlines t)
+(delete-selection-mode 1)
+(global-visual-line-mode t)
+(show-paren-mode t)
+
+;; Packages
+;;; General - More powerful keybinding functions
 (use-package general) 
 (general-define-key
  "C-M-j" 'counsel-switch-buffer)
 
-;;ensure that all needed packages are installed
-(setq use-package-always-ensure t)
+;;; Lispy - for better elisp editing
+(use-package lispy)
+(add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode t))) 
 
-;;initialize package sources
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-
-;;enable use-package
-(require 'use-package)
- 
-;;Disable splash screen and banner
-(setq inhibit-startup-message t
-      inhibit-startup-echo-area-message t) 
-
-;;Enable Sublimity
-(add-to-list 'load-path "~/.emacs.d/sublimity/")
-(require 'sublimity)
+;;; Sublimity - Smoother scrolling
+(use-package sublimity)
 (require 'sublimity-scroll)
-(sublimity-mode 1)
+(sublimity-mode t)
 
-;;disable line numbering in eshell and org mode
-(add-hook 'eshell-mode-hook (lambda () (display-line-numbers-mode 0))) 
-(add-hook 'org-mode-hook (lambda () (display-line-numbers-mode 0)))
+;;; Ivy - A generic completion mechanism
+(use-package ivy
+  :config
+  (ivy-mode 1)
+  :bind (("C-s" . swiper)))
 
-;;Enable IDO
-;;(ido-mode 1)
-;;(setq ido-everywhere t)
-;;(setq ido-enable-flex-matching t)
-;;(setq ido-file-extensions-order '(".tex"))
-
-;;Enable diminish
-(use-package diminish)
-
-;;enable counsel
+;;; Counsel - A collection of Ivy-enhanced versions of common Emacs commands
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
@@ -56,26 +63,19 @@
 	 ("C-r" . 'counsel-minibuffer-history))
   :config (setq ivy-initial-inputs-alist nil))
 
-;;Enable IVY and swiper
-(use-package swiper)
-(use-package ivy
-  :config
-  (ivy-mode 1)
-  :bind (("C-s" . swiper)))
-
-;;enable ivy-rich
+;;; Ivy-rich - A more friendly interface for ivy
 (use-package ivy-rich
   :init (ivy-rich-mode 1))
 
-;;don't show ivy-mode in modeline
-(diminish 'ivy-mode)
+;;; Swiper - An Ivy-enhanced search tool
+(use-package swiper)
 
-;;enable which-key
+;;; Which-key - A minor-mode which displays avalable keybindings
 (use-package which-key
   :init (which-key-mode)
   :config (setq which-key-idle-delay 0.3))
 
-;;enable helpful
+;;; Helpful - A more helpful help page
 (use-package helpful
   :custom
   (counsel-describe-function-function #'helpful-callable)
@@ -86,45 +86,20 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-;;enable doom-modeline
-(use-package doom-modeline :ensure t
+;;; Doom-modeline - A fancier modeline
+(use-package doom-modeline
   :init (doom-modeline-mode 1))
-
-;;enable doom-themes
-(use-package doom-themes)
-
-;;Finish redrawing of display, before input events are handled to reduce screen tearing
-;;(setq redisplay-dont-pause t) Turned off becauso it caused strange flickering
-
-;;Insert new lines when reaching bottom
-(setq next-line-add-newlines t)
-
-;;Turn line numbers mode on by default
-(global-display-line-numbers-mode 1)
-
-;;Don't wrap in the middle of a word
-(global-visual-line-mode t)
-
-;;Change cursor style to line, instead of block
-(setq-default cursor-type 'bar) 
-
-;;Set cursor color to white
-(set-cursor-color "#ffffff")
-
-;;Replace selection when typing
-(delete-selection-mode 1)
-
-;;enable all-the-icons
 (use-package all-the-icons)
-
-(require 'xah-fly-keys)
-
-;(xah-fly-keys-set-layout 'koy)
-
-;(xah-fly-keys 1)
-
-;;define default directory
-(setq default-directory "~/Documents/4ahme/")
-
-(put 'upcase-region 'disabled nil)
-(put 'downcase-region 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(doom-modeline helpful which-key ivy-rich sublimity lispy general xah-fly-keys use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
